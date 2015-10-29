e033573df49d920360f83536f7cdc4d2b59fe4e8
Alejandro Druetta <aledruetta@gmail.com>
2015-10-29 20:12:21 -0200

    Sketch directory
        - Adicionar directório para o sketch robot.ino
        - Estilo comentários
        - Estilo indentação
        - const vel e literal 180 --> VEL
        - Adicionar .gitignore
    
    Changes to be committed:
    	new file:   .gitignore
    	deleted:    Robot.ino
    	renamed:    robot.ino -> robot/robot.ino

--- robot/robot.ino
+++ robot/robot.ino
@@ -4,25 +4,28 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
-/* Assign a unique ID to this sensor at the same time */
+// Assign a unique ID to this sensor at the same time
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
+#define BEEP 16
+
+// Define pinos Ultrasonic Sensor HC-SRO4
 #define TRIGGER_PIN  15
 #define ECHO_PIN     14
-#define BEEP         16
-
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
 AF_DCMotor motorE(1); 
 AF_DCMotor motorD(2);
 
+boolean indoPraFrente = false;
 boolean som = false;
-boolean obstaculo;
+boolean obstaculo = false;
 
 const int margemAngulo = 3;
 int anguloAlvo = 0;
 int anguloAtual = 0;
 
-const int vel = 180;
+const int VEL = 180;
 
 int velocidadeE = 0;
 int velocidadeD = 0;
@@ -36,54 +39,64 @@ void displaySensorDetails(void) {
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");
   Serial.println("------------------------------------");
   Serial.println("");
   delay(500);
 }
 
-int verificarDistancia() {
+/* Ultrasonic Sensor - Retorna distancia obstáculo em centímetros */
+  int verificarDistancia(){
   float cmMsec;
   long microsec = ultrasonic.timing();
 
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
-    
+
   Serial.print("CM: ");
   Serial.println(cmMsec);  
   if (cmMsec < 60) {
-       Serial.println(microsec);
+    Serial.println(microsec);
   }
   return (int) cmMsec; 
 }
 
-void andarPraFrente() {
-  motorE.setSpeed(velocidadeE);   //Define a velocidade maxima
-  motorE.run(FORWARD);            //Gira o motor sentido horario
-  motorD.setSpeed(velocidadeD);   //Define a velocidade maxima
-  motorD.run(FORWARD);            //Gira o motor sentido horario
-  delay(100);
+void andarPraFrente(){
+  //if (!indoPraFrente) {
+    motorE.setSpeed(velocidadeE);   //Define a velocidade maxima
+    motorE.run(FORWARD);            //Gira o motor sentido horario
+    motorD.setSpeed(velocidadeD);   //Define a velocidade maxima
+    motorD.run(FORWARD);            //Gira o motor sentido horario
+    indoPraFrente = true;
+    delay(100);
+  //}
 }
 
-void darRe(int tempo) {
-  motorE.setSpeed(vel);       //Define a velocidade maxima
-  motorE.run(BACKWARD);       //Gira o motor sentido horario
-  motorD.setSpeed(vel);       //Define a velocidade maxima
-  motorD.run(BACKWARD);       //Gira o motor sentido horario
-  delay(tempo);
-  parar();
+void darRe(int tempo){
+  //if (!indoPraFrente) {
+    motorE.setSpeed(VEL);   //Define a velocidade maxima
+    motorE.run(BACKWARD);   //Gira o motor sentido horario
+    motorD.setSpeed(VEL);   //Define a velocidade maxima
+    motorD.run(BACKWARD);   //Gira o motor sentido horario
+    //indoPraFrente = true;
+    delay(tempo);
+    parar();
+  //}
 }
 
-void parar() {
-  motorE.setSpeed(0); 
-  motorE.run(RELEASE);
-  motorD.setSpeed(0); 
-  motorD.run(RELEASE);
+void parar(){
+  //if (indoPraFrente) {
+    motorE.setSpeed(0);
+    motorE.run(RELEASE);
+    motorD.setSpeed(0);
+    motorD.run(RELEASE);
+    indoPraFrente = false;
+  //}
 }
 
 void virarDireita() {
-  motorE.setSpeed(vel); 
+  motorE.setSpeed(VEL); 
   motorE.run(FORWARD);
-  motorD.setSpeed(vel); 
+  motorD.setSpeed(VEL); 
   motorD.run(BACKWARD);
   delay(400);
   parar();  
@@ -96,36 +109,41 @@ void setup() {
   pinMode(BEEP,OUTPUT);
 
   /* Initialise the sensor */
-  if(!mag.begin()) {
-    /* There was a problem detecting the HMC5883 ... check your connections */
+  if(!mag.begin())
+  {
+    // There was a problem detecting the HMC5883 ... check your connections
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
   }
-  
-  /* Display some basic information on this sensor */
+
+  // Display some basic information on this sensor
   displaySensorDetails();
   
-  velocidadeE = vel;
-  velocidadeD = vel;
+  velocidadeE = VEL;
+  velocidadeD = VEL;
   
   anguloAlvo = pegarBussola();
 }
 
 void loop() {
   if (!som) {
-    analogWrite(BEEP,255);
+    analogWrite(BEEP, 255);
     som = true;
   } else {
-    analogWrite(BEEP,0);
+    analogWrite(BEEP, 0);
     som = false;
   }
   
   anguloAtual = pegarBussola();
   calcDesiredTurn();
-  
-  if (verificarDistancia() < 25) obstaculo = true;
-  else obstaculo = false;
-  
+
+  //if (verificarDistancia() < 25) {
+    if (verificarDistancia() < 25) {
+      if (verificarDistancia() < 25) obstaculo = true;
+      else obstaculo = false;
+    } else obstaculo = false;
+  //} else obstaculo = false;
+
   if (obstaculo) {
     //virarDireita
     parar();
@@ -139,15 +157,15 @@ void loop() {
     //andarPraFrent
     andarPraFrente();
   }
- delay(100); 
+  delay(100);
 }
 
 int pegarBussola() {
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
+  // Get a new sensor event
+  sensors_event_t event;
   mag.getEvent(&event);
- 
-  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+
+  // Display the results (magnetic vector values are in micro-Tesla (uT))
   Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
   Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
   Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
@@ -155,57 +173,58 @@ int pegarBussola() {
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
   float heading = atan2(event.magnetic.x, event.magnetic.y); //referência é o eixo y!!
-  
+
   // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
   // Find yours here: http://www.magnetic-declination.com/
   // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
   // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
   float declinationAngle = -0.38; // como mudou a referência, utilizamos a declinação negativa.
-  
+
   heading += declinationAngle;
-  
+
   // Correct for when signs are reversed.
   if(heading < 0)
     heading += 2*PI;
-    
+
   // Check for wrap due to addition of declination.
   if(heading > 2*PI)
     heading -= 2*PI;
-   
+
   // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
-  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+  float headingDegrees = heading * 180/M_PI;
+
+  Serial.print("Heading (degrees): ");
+  Serial.println(headingDegrees);
   return (int)headingDegrees;
 }
 
-void calcDesiredTurn(void) {
+void calcDesiredTurn(void)
+{
   // calculate where we need to turn to head to destination
   int erroAtual = anguloAlvo - anguloAtual;
 
   // adjust for compass wrap
-  if (erroAtual < -180)      
+  if (erroAtual < -180)
     erroAtual += 360;
   if (erroAtual > 180)
     erroAtual -= 360;
 
   // calculate which way to turn to intercept the targetHeading
-  if (abs(erroAtual) <= margemAngulo) {      // if within tolerance, don't turn
-    velocidadeD = vel;
-    velocidadeE = vel;
+  // if within tolerance, don't turn
+  if (abs(erroAtual) <= margemAngulo) {      
+    velocidadeD = VEL;
+    velocidadeE = VEL;
   }
   else if (erroAtual < 0) {
-    velocidadeE = vel - 30;
-    velocidadeD = vel;
+    velocidadeE = VEL - 30;
+    velocidadeD = VEL;
   }
   else if (erroAtual > 0) {
-    velocidadeD = vel - 30;
-    velocidadeE = vel;
+    velocidadeD = VEL - 30;
+    velocidadeE = VEL;
   }
   else {
-    velocidadeD = vel;
-    velocidadeE = vel;
+    velocidadeD = VEL;
+    velocidadeE = VEL;
   }
- 
-}  // calcDesiredTurn()
-
+}
